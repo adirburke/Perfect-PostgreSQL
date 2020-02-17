2140e4e55f853688719dd7cfe0ba004ced3e8190
Adir Burke on Laptop <adir@hanave.com>
2020-02-17 16:13:46 +1100

    :{

--- Package.swift
+++ Package.swift
@@ -44,7 +44,7 @@ let package = Package(
 		.library(name: "PerfectPostgreSQL", targets: ["PerfectPostgreSQL"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-CRUD.git", from: "2.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-CRUD.git", from: "2.0.0-alpha01"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-libpq-linux.git", from: "2.0.0"),
 		],
 	targets: [
