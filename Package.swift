08b19f946f6e36f9ec1ef551be40e1394ef8aaca
Adir Burke on Laptop <adir@hanave.com>
2020-09-18 11:31:44 +1000

    Fixed room swap added alot of logging

--- Package.swift
+++ Package.swift
@@ -44,7 +44,7 @@ let package = Package(
 		.library(name: "PerfectPostgreSQL", targets: ["PerfectPostgreSQL"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/adirburke/Perfect-CRUD.git", from: "2.0.0-alpha4"),
+		.package(path: "../Perfect-CRUD"),
 		
 		.package(url: "https://github.com/PerfectlySoft/Perfect-libpq-linux.git", from: "2.0.0"),
 		],
